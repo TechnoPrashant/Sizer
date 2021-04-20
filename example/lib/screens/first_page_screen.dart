371876fc6258dc9f89bce51439b74ced08b2bec8
admin <urmishpatel9@gmail.com>
2021-04-20 17:19:05 +0530

    * Fix all bugs * Update with flutter 2.0 features * Support to web and desktop

--- example/lib/screens/first_page_screen.dart
+++ example/lib/screens/first_page_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
 
-
 class FirstPageScreen extends StatefulWidget {
   @override
   _FirstPageScreenState createState() => _FirstPageScreenState();
